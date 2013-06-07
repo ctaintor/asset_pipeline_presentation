b764ea663a8ec8c0ad8fdc2413b3aba179d87c1f
Case Taintor <case.taintor@klarna.com>
2013-06-07 15:26:17 +0300

    switches to using evaluate()

--- config/initializers/caseable_language.rb
+++ config/initializers/caseable_language.rb
@@ -2,17 +2,10 @@ require 'tilt/template'
 module Tilt
   class CaseableTemplate < Template
     def prepare
-      converted_text = data.gsub(/(\s)case([;\s])/, '\1casevar\2').gsub(/when/, "case")
-      @code = "<<#{hash}.chomp\n#{converted_text}\n#{hash}"
     end
 
-    def precompiled_template(locals)
-      @code
-    end
-
-    def precompiled(locals)
-      source, offset = super
-      [source, offset + 1]
+    def evaluate(scope, locals, &block)
+      data.gsub(/(\s)case([;\s])/, '\1casevar\2').gsub(/when/, "case")
     end
   end
 end
