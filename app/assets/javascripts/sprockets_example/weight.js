035c6e5c2d240bf2816f53ec0ed477985b911117
Case Taintor <case.taintor@klarna.com>
2013-08-24 15:35:31 +0300

    Resetting to the beginning of the talk, with some changes

--- app/assets/javascripts/sprockets_example/weight.js
+++ app/assets/javascripts/sprockets_example/weight.js
@@ -1,13 +1,6 @@
-//= require sprockets_example/giraffe
-//= require sprockets_example/zebra
-
 Weight = {
   avgGiraffeWeight: Giraffe.weight,
-  avgZebraWeight: Zebra.weight,
   convertToGiraffes: function (kilograms) {
     return kilograms / this.avgGiraffeWeight;
-  },
-  convertToZebras: function (kilograms) {
-    return kilograms / this.avgZebraWeight;
   }
 };
